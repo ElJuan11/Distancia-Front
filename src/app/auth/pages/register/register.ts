38ffb72d14987cdc1e713923a6b3fb07661d1dfb
ElJuan11 <juanpabloalavarado@gmail.com>
2025-06-26 12:52:40 -0500

    Register y service auth

--- src/app/auth/pages/register/register.ts
+++ src/app/auth/pages/register/register.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { registerRequest } from '../../auth-module';
+import { Auth } from '../../services/auth';
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule  ],
   templateUrl: './register.html',
   styleUrl: './register.css'
+  
 })
-export class Register {
 
+export class Register { 
+  formData: registerRequest = {
+    nombre: '',
+    apellidos: '',
+    edad: '0',
+    sexo: '',
+    localidad: '',
+    numTel: '000000',
+    correo: '',
+    ocupacion: '',
+    password: ''
+  };
+
+    constructor(private authService: Auth) {}
+
+ onRegister() {
+ console.log("Objeto:", this.formData);  // Imprimir como objeto
+    console.log("JSON:", JSON.stringify(this.formData));  // Imprimir como JSON string
+
+    const respuesta = this.authService.register(this.formData)
+  }
 }
