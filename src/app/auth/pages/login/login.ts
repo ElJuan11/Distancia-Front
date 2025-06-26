738d9429e303ca5efd6b112068c5d4a72a56fa15
elguachiloco69 <pirata.09.dalmi@gmail.com>
2025-06-26 13:14:45 -0500

    Login (#2)
    * Login
    
    * Login funciona

--- src/app/auth/pages/login/login.ts
+++ src/app/auth/pages/login/login.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import {Component} from "@angular/core";
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {FormsModule} from "@angular/forms";
+import { Auth } from "../../services/auth";
+import { loginRequest } from "../../auth-module";
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule ] ,
   templateUrl: './login.html',
   styleUrl: './login.css'
+
+
 })
 export class Login {
 
+  constructor(
+    private authService:Auth
+  ){}
+
+
+nocontrol :string =  ""
+Password :string =  ""
+
+
+ onLogin(){
+  const datos: loginRequest ={
+    correo: this.nocontrol,
+    password:  this.Password,
+  };
+
+  const respuesta =  this.authService.login(datos)
+  console.log(respuesta);
+  
+
 }
+}
+
--- src/app/auth/services/auth.ts
+++ src/app/auth/services/auth.ts
@@ -1,17 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { registerRequest } from '../auth-module';
+import { loginRequest, registerRequest } from '../auth-module';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
-  private registerUrl = 'http://localhost:3000/users/register';
+  private backendUrl = 'http://localhost:3000';
 
   constructor(private http: HttpClient) {}
 
   register(data: registerRequest) {
-    return this.http.post(this.registerUrl, data).subscribe(
+    return this.http.post(`${this.backendUrl}/users/register`, data).subscribe(
       {
         next: (res) => {
           console.log(res)
@@ -23,4 +23,20 @@ export class Auth {
       }
     );
   }
+
+  login(data: loginRequest) {
+    return this.http.post(`${this.backendUrl}/users/login`, data).subscribe(
+      {
+        next: (res) => {
+          console.log(res)
+          return res
+        },
+        error: (err) => {
+          console.error('Error en el registro', err);
+        }
+      }
+    );
+  }
+
+
 }
