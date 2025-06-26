d8cb126d52984375be29de5df511b25c2bb26e00
elguachiloco69 <pirata.09.dalmi@gmail.com>
2025-06-26 13:01:24 -0500

    Login

--- src/app/auth/pages/login/login.ts
+++ src/app/auth/pages/login/login.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import {Component} from "@angular/core";
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {FormsModule} from "@angular/forms";
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule ] ,
   templateUrl: './login.html',
   styleUrl: './login.css'
+
+
 })
 export class Login {
 
+
+
+nocontrol :string =  ""
+Password :string =  ""
+
+
+onLogin(){
+  const datos ={
+    noControl: this.nocontrol,
+    Password:  this.Password,
+  };
+
+  console.log("Objeto:", datos ); // como objeto 
 }
+}
+
